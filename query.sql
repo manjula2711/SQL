31d412f1be5b86a36d91a212cdc98f53d16a5ee9
manjula2711 <130962788+manjula2711@users.noreply.github.com>
2023-04-28 07:31:44 +0530

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,21 @@
+create database HeroVired;
+create table RGMCET(
+Id_no int primary key not null,
+SName varchar(30) not null,
+college varchar(20) not null,
+Session1 varchar(30) not null,
+contact_no int not null,
+address varchar(40) not null
+);
+INSERT INTO RGMCET VALUES (101 , "dinesh" , "RGM" ,"SQL" ,987692455,"Nandyal");
+SELECT * from RGMCET;
+INSERT INTO RGMCET VALUES (102 , "akhila" , "RGM" ,"SQL" ,987695455,"Nandyal");
+INSERT INTO RGMCET VALUES (103 , "manjula" , "RGM" ,"SQL" ,987632455,"Nandyal");
+INSERT INTO RGMCET VALUES (104 , "chandu" , "RGM" ,"SQL" ,987691455,"Nandyal");
+INSERT INTO RGMCET VALUES (105 , "manju" , "RGM" ,"SQL" ,987112455,"Nandyal");
+INSERT INTO RGMCET VALUES (106 , "swathi" , "RGM" ,"SQL" ,982292455,"Nandyal");
+INSERT INTO RGMCET VALUES (107 , "swapna" , "RGM" ,"SQL" ,988892455,"Nandyal");
+INSERT INTO RGMCET VALUES (108 , "uma" , "RGM" ,"SQL" ,987692335,"Nandyal");
+INSERT INTO RGMCET VALUES (109 , "rishik" , "RGM" ,"SQL" ,987292455,"Nandyal");
+INSERT INTO RGMCET VALUES (110 , "harish" , "RGM" ,"SQL" ,987192455,"Nandyal");
+
